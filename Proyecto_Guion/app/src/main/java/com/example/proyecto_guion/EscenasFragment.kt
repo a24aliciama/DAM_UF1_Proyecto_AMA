5d852e0244d81712236f4ade595cae00755b762b
A24AliciaMA <a24aliciama@a26eql09.sanclemente.local>
2024-11-26 10:29:19 +0100

    menu

--- Proyecto_Guion/app/src/main/java/com/example/proyecto_guion/EscenasFragment.kt
+++ Proyecto_Guion/app/src/main/java/com/example/proyecto_guion/EscenasFragment.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class EcenasFragment : Fragment() {
+class EscenasFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ecenas, container, false)
+        return inflater.inflate(R.layout.fragment_escenas, container, false)
     }
 }
